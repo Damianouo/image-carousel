a8f686529c4891740f96b7160e04e9daebc8c6ae
Damianouo <damianouo0@gmail.com>
2025-06-19 19:09:04 +0800

    fix: Replace outdated image URLs

--- src/data.js
+++ src/data.js
@@ -1,27 +1,27 @@
 const pic = [
   {
-    url: 'https://source.unsplash.com/AJ_Mou1FUS8',
-    alt: 'AJ_Mou1FUS8',
+    url: 'https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=400',
+    alt: 'photo-1524504388940-b1c1722653e1?ixlib',
   },
   {
-    url: 'https://source.unsplash.com/0V7_N62zZcU',
-    alt: '0V7_N62zZcU',
+    url: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=400',
+    alt: 'photo-1534528741775-53994a69daeb',
   },
   {
-    url: 'https://source.unsplash.com/abkEAOjnY0s',
-    alt: 'abkEAOjnY0s',
+    url: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=400',
+    alt: 'photo-1506794778202-cad84cf45f1d',
   },
   {
-    url: 'https://source.unsplash.com/1vzLW-ihJaM',
-    alt: '1vzLW-ihJaM',
+    url: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=400',
+    alt: 'photo-1544005313-94ddf0286df2',
   },
   {
-    url: 'https://source.unsplash.com/DKix6Un55mw',
-    alt: 'DKix6Un55mw',
+    url: 'https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?w=400',
+    alt: 'photo-1531746020798-e6953c6e8e04',
   },
   {
-    url: 'https://source.unsplash.com/vEE00Hx5d0Q',
-    alt: 'vEE00Hx5d0Q',
+    url: 'https://images.unsplash.com/photo-1521119989659-a83eee488004?w=400',
+    alt: 'photo-1521119989659-a83eee488004',
   },
 ];
 
